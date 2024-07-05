665dfa61b7dd24b9789d7d81b12f09e8e2741807
Alston <alston.hou@gmail.com>
2024-07-04 22:43:14 -0400

    created base files in order to send trade requests to api

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import alpaca_trade_api as tradeapi
+import requests
+import config
+from config import *
+
+ORDERS_URL = '{}/v2/orders'.format(config.APCA_API_BASE_URL)
+
+def createMarketOrder():
+    ticker = 'AAPL'
+    qty = '2'
+    side = 'buy'
+    ordertype = 'market'
+
+    data = {
+        'symbol' : ticker,
+        'qty' : qty,
+        'side' : side,
+        'type' : ordertype,
+        'time_in_force' : 'day'
+    }
+
+    r = requests.post(ORDERS_URL, json=data, headers=config.HEADERS)
+    return r.content
+
+def createLimitOrder():
+    ticker = 'AAPL'
+    qty = '2'
+    side = 'buy'
+    ordertype = 'market'
+    limitPrice = '140'
+
+    data = {
+        'symbol' : ticker,
+        'qty' : qty,
+        'side' : side,
+        'type' : ordertype,
+        'time_in_force' : 'day',
+        'limit_price' : limitPrice
+    }
+
+    r = requests.post(ORDERS_URL, json=data, headers=config.HEADERS)
+    return r.content
+print(createMarketOrder())
