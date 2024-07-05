135e4c14295da4735ca160d3145e8f8dac496eb9
Alston Hou <alston.hou@gmail.com>
2024-07-05 16:51:04 -0400

    created a max historical price function

--- src/handleTrades.py
+++ src/handleTrades.py
@@ -2,18 +2,33 @@ import alpaca_trade_api as tradeapi
 import requests
 import config
 import orderController
+import sys
 
-def shouldBuy():
+api = tradeapi.REST(
+    key_id = config.ALPACA_KEY,
+    secret_key = config.ALPACA_SECRET_KEY,
+    base_url = config.APCA_API_BASE_URL
+)
+
+def findHistoricMax(stock = ''):
+    max = -1
+    barset = api.get_barset(stock, 'day', limit = sys.maxint)
+    for data in barset:
+        if data > max:
+            max = data
+    return max
+
+def shouldBuy(stock = ''):
     return True
 
-def shouldSell():
+def shouldSell(stock = ''):
     return True
 
 def handleStock(stock = ''):
-    if shouldBuy:
+    if shouldBuy(stock):
         orderController.createMarketOrder(stock)
         return 1
-    elif shouldSell:
+    elif shouldSell(stock):
         orderController.sellMarketOrder(stock)
         return 1
     return 0
